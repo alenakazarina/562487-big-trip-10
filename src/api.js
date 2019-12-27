9660bef4a01ea7dd2ecbf1e719dbede7b61303e0
Alena Kazarina <alenakazarina1@gmail.com>
2019-12-27 20:21:43 +0300

    added api methods

--- src/api.js
+++ src/api.js
@@ -1,4 +1,6 @@
 import Point from './models/point';
+import Destination from './models/destination';
+import Offer from './models/offer';
 
 const Method = {
   GET: `GET`,
@@ -20,25 +22,51 @@ class API {
     this._authorizatioin = authorization;
   }
 
-  getTasks() {
+  getPoints() {
     return this._load({url: `points`})
               .then((response) => response.json())
               .then(Point.parsePoints);
   }
 
-  createTask() {
-
+  createPoint(point) {
+    return this._load({
+      url: `points`,
+      method: Method.POST,
+      body: JSON.stringify(point.toRAW()),
+      headers: new Headers({'Content-Type': `application/json`})
+    })
+      .then((response) => response.json())
+      .then(Point.parsePoint);
   }
 
-  updateTask() {
-
+  updatePoint(id, data) {
+    return this._load({
+      url: `points/${id}`,
+      method: Method.PUT,
+      body: JSON.stringify(data.toRAW()),
+      headers: new Headers({'Content-Type': `application/json`})
+    })
+      .then((response) => response.json())
+      .then(Point.parsePoint);
   }
 
-  deleteTask() {
-
+  deletePoint(id) {
+    return this._load({url: `points/${id}`, method: Method.DELETE});
   }
 
-  _load(url, method = Method.GET, body = null, headers = new Headers()) {
+  getDestinations() {
+    return this._load({url: `destinations`})
+              .then((response) => response.json())
+              .then(Destination.parseDestinations);
+  }
+
+  getOffers() {
+    return this._load({url: `offers`})
+              .then((response) => response.json())
+              .then(Offer.parseOffers);
+  }
+
+  _load({url, method = Method.GET, body = null, headers = new Headers()}) {
     headers.append(`Authorization`, this._authorizatioin);
 
     return fetch(`${this._endPoint}/${url}`, {method, body, headers})
